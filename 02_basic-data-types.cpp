dbedfe58ba0c0eae3aabb46cf8dc396def6452c6
CharlyCeballos <carlos.ceballos@gometrix.dev>
2024-05-16 13:00:04 -0600

    02 Tipos de datos básicos

---
+++ 02_basic-data-types.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int integerNumber = 15;
+  float floatNumber = 95.23;
+  double doubleNumb = 7896321453698745214532.98752145;
+  char character = 'C';
+
+  cout<<integerNumber<<endl;
+  cout<<floatNumber<<endl;
+  cout<<doubleNumb<<endl;
+  cout<<character<<endl;
+  return 0;
+}
