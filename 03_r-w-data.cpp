84f68c01d99f2914ad797ca3cebc3ff65373422f
CharlyCeballos <carlos.ceballos@gometrix.dev>
2024-05-16 13:08:40 -0600

    03 Read/Write data entry

---
+++ 03_r-w-data.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int integerNumber;
+
+  cout<<"Digite un numero entero: ";
+  cin>>integerNumber;
+  cout<<endl<<"El numero entero que usted digito es: "<<integerNumber;
+
+  return 0;
+}
