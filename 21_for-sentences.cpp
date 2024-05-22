8520e068e8f4184abcbbfd5e4839a7b775f3edf4
CharlyCeballos <carlos.ceballos@gometrix.dev>
2024-05-22 14:55:23 -0600

    21 FOR sentences

---
+++ 21_for-sentences.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int main() {
+  int i = 0;
+
+  for (i = 1; i < 11; i++) {
+    cout<<endl<<i;
+  }
+
+  cout<<endl;
+
+  for (i = 10; i > 0; i--) {
+    cout<<endl<<i;
+  }
+
+  getch();
+  return 0;
+}
