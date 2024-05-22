c4018530a592d83ed70a96ed22ab727baba962fe
CharlyCeballos <carlos.ceballos@gometrix.dev>
2024-05-22 17:25:40 -0600

    30 ARRAYS unidimensional

---
+++ 30_array-unidimensional.cpp
@@ -0,0 +1,17 @@
+/*
+Los arreglos se definen como => type name[length]
+Ejemplo:                Inicialización:
+int numbers[10];        int numbers[] = { -1, 0, 3, 4, 5 };
+char letters[5];        char letters[] = { 'a', 'e', 'i', 'o', 'u' };
+float reals[15];        float reals[] = { 1, 2, 3, 4, 5 };
+*/
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int main() {
+  cout<<"Hello everybody out there";
+
+  getch();
+  return 0;
+}
