f3d30f6af917ffc61102c6044c2880d6f44e231f
CharlyCeballos <carlos.ceballos@gometrix.dev>
2024-05-29 13:58:36 -0600

    36 ARRAYS bidimensionals

---
+++ 36_array-bidimensional.cpp
@@ -0,0 +1,34 @@
+/*
+Los arreglos se definen como => type name[length]
+Ejemplo:                          Inicialización:
+int numbers[rows][colums];        int numbers[rows][colums] = {1,2,3
+                                                               a,b,c};
+*/
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int main() {
+  int numbers[5][10], asciiLetter = 65;
+
+  printf("Salida ASCII");
+  cout<<"\n";
+  cout<<"\n";
+
+  for (int i = 0; i < 5; i++) {
+    for (int j = 0; j < 10; j++) {
+      numbers[i][j] = asciiLetter;
+      asciiLetter++;
+    }
+  }
+
+  for (int i = 0; i < 5; i++) {
+    for (int j = 0; j < 10; j++) {
+      printf("%c ", numbers[i][j]);
+    }
+    cout<<"\n";
+  }
+
+  getch();
+  return 0;
+}
