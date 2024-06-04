df35d46f49487845d756fa24c6269e5f637ad66b
CharlyCeballos <carlos.ceballos@gometrix.dev>
2024-06-04 10:46:56 -0600

    55 Struct

---
+++ 55_Struct.cpp
@@ -0,0 +1,31 @@
+/*
+Una estructura es una colección de uno o más tipos de elementos denominado campos,
+cada uno de los cuales puede ser un tipo de dato diferente.
+
+Ejemplo:
+  struct <structureName> {
+    <dataType> <fieldName>;
+    <dataType> <fieldName>;
+    ...
+    <dataType> <fieldName>;
+  }
+  struct collection_CD {
+    char title[30];
+    char artist[25];
+    int numSongs;
+    float price;
+    char purchaseDate[20];
+  }CD1, CD2, CD3;
+
+*/
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int main() {
+
+  cout<<"Hello everybody out there";
+
+  getch();
+  return 0;
+}
